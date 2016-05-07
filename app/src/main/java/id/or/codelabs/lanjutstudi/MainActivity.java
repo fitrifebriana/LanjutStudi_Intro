fd7e4202956fe60a6979d6cbc6b4e07b417ee367
Fitri Febriana <fitfebriana@gmail.com>
2016-05-07 13:02:46 +0700

    Initial commit

---
+++ app/src/main/java/id/or/codelabs/lanjutstudi/MainActivity.java
@@ -0,0 +1,14 @@
+package id.or.codelabs.lanjutstudi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
